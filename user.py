5e4175ae83073a90603c3578cc9466488a89ac7e
dhirajpanhare <66125987+dhirajpanhare@users.noreply.github.com>
2020-08-12 20:45:40 +0530

    Add files via upload

---
+++ user.py
@@ -0,0 +1,21 @@
+name = input("Enter your name : ")
+
+print("Hello" , name)
+print ("LET'S Multiply numbers")
+num1 = int(input("enter your 1st number" ) )
+
+num2 = int(input("enter tour 2nd number") )
+
+num3 = num1 * num2
+
+print("Answer" , num3)
+
+print ("LET'S Adding Numbers")
+
+one = (input("enter your1st number") )
+two = (input("enter your2nd number") )
+
+three = one * two
+
+print ("answer" , three)
+
